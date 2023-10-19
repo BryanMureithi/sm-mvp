2c0b8205301ee426a9b8efaf4d2817d548d0d158
BryanMureithi <brymureithi@gmail.com>
2023-10-19 15:18:17 +0300

    Installed dependencies

---
+++ sortseam/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import NavBar from "./components/NavBar.vue";
+
+export default {
+  name: "App",
+  components: { NavBar },
+};
+</script>
+
+<template>
+  <div class="home">
+    <NavBar />
+  </div>
+</template>
+
+<style scoped>
+.home {
+  background-color: #1f1f22;
+}
+</style>
---
+++ sortseam/src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <nav
+      class="flex justify-between px-24 py-3 bg-transparent items-center shadow-sm sticky top-0 z-10"
+    >
+      <img src="src\assets\images\logo.png" alt="" />
+      <div class="flex space-x-6 text-white">
+        <RouterLink
+          to="/"
+          class="hover:text-[#D90429] text-base font-inter text-md cursor-pointer hover:text-[#5454D4]"
+          >Home</RouterLink
+        >
+        <RouterLink
+          to="/"
+          class="hover:text-[#D90429] text-base font-inter text-md cursor-pointer"
+          >About Us</RouterLink
+        >
+        <RouterLink
+          to="/"
+          class="hover:text-[#D90429] text-base font-inter text-md cursor-pointer"
+          >What We Do</RouterLink
+        >
+        <RouterLink
+          to="/"
+          class="hover:text-[#D90429] text-base font-inter text-md cursor-pointer"
+          >Our Work</RouterLink
+        >
+      </div>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
